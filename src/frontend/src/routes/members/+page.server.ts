643cef9575327058ad50c06b4328e9f3e2ea47b5
Firdaus Kamaruddin <mail4frenz@gmail.com>
2025-04-19 20:12:20 +0800

    remove svelte

--- src/frontend/src/routes/members/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import type {ServerLoadEvent} from "@sveltejs/kit";
-import {apis} from "$lib/config";
-
-export async function load({ fetch }: ServerLoadEvent) {
-    const response = await fetch(`${apis.member}/members`);
-    const members = await response.json();
-    return { members }
-}
